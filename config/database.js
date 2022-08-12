f8ebc051e77eb51f957f82963b1b82ab9373f63b
sampaulino23 <61399155+sampaulino23@users.noreply.github.com>
2022-08-13 00:25:37 +0800

    Revert "fix req.isAuthenticated"
    This reverts commit c5f2d7b985ae83a3ecbf22974269471b9b477d57.

--- config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    database: 'mongodb+srv://test:test@cluster0.tcgdc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
--- index.js
+++ index.js
@@ -2,32 +2,15 @@ const express = require('express')
 const bodyparser = require('body-parser')
 const handlebars = require('handlebars');
 var urlencodedparser = bodyparser.urlencoded({extended:false});
-const path = require('path');
-const exphbs = require('express-handlebars');
 
 const app = express();
 
-app.engine( 'hbs', exphbs.engine({
-    extname: 'hbs', // configures the extension name to be .hbs instead of .handlebars
-    defaultView: 'main', // this is the default value but you may change it to whatever you'd like
-    layoutsDir: path.join(__dirname, '/views/layouts'), // Layouts folder
-    partialsDir: path.join(__dirname, '/views/partials'), // Partials folder
-    
-}));
-
-app.set('view engine', 'hbs');
+app.set('view engine', handlebars);
 app.use(express.static('public'));
 app.use(bodyparser.json());
 
-
-
-
-
-
-//Import Routes
-
-const loginRoute = require('./routes/login');
-app.use('/', loginRoute);
-
+app.use(function(req, res) {
+    res.send('OULC Contract Management System');
+});
 
 app.listen(process.env.PORT || 3000);
