01cd05e0022de773d7a0fe16f5f7f96105facd06
brosoto-cmd <mig_franz_brosoto@dlsu.edu.ph>
2022-08-17 10:56:10 +0800

    Search and Filter Table
    - Need frontend fix
    - To clarify dropdown selections

---
+++ public/js/tablefilter.js
@@ -0,0 +1,39 @@
+//TABLE FILTER FOR ADMIN...
+
+function filterUserDepartment(){ //for inventory (product category) and purchasing (product category)
+    var dropdown = document.getElementById("departmentdropdown");
+    var table = document.getElementById("table");
+    var rows = table.getElementsByTagName("tr");
+    var filter = dropdown.value;
+
+    for (let row of rows) { // `for...of` loops through the NodeList
+        cells = row.getElementsByTagName("td");
+        department = cells[2] || null; // gets the 2nd `td` or nothing
+        // if the filter is set to 'All', or this is the header row, or 2nd `td` text matches filter
+        if (filter === "All" || !department || (filter === department.textContent)) {
+            row.style.display = ""; // shows this row
+        }
+        else {
+            row.style.display = "none"; // hides this row
+        }
+    }
+}
+
+function filterUserRole(){ //for inventory (product category) and purchasing (product category)
+    var dropdown = document.getElementById("roledropdown");
+    var table = document.getElementById("table");
+    var rows = table.getElementsByTagName("tr");
+    var filter = dropdown.value;
+
+    for (let row of rows) { // `for...of` loops through the NodeList
+        cells = row.getElementsByTagName("td");
+        role = cells[3] || null; // gets the 3rd `td` or nothing
+        // if the filter is set to 'All', or this is the header row, or 2nd `td` text matches filter
+        if (filter === "All" || !role || (filter === role.textContent)) {
+            row.style.display = ""; // shows this row
+        }
+        else {
+            row.style.display = "none"; // hides this row
+        }
+    }
+}
