cb5f06dd12a370be3f599901a403a0d3418c5c72
Ze Long Zhuang <74848471+ZeLongZhuang@users.noreply.github.com>
2022-09-13 21:23:23 +0800

    Remove use of crypo in naming files

--- routes/staff.js
+++ routes/staff.js
@@ -62,21 +62,34 @@ conn.once('open',() => {
 });
 
 //create storage object
+// const storage = new GridFsStorage({
+//   db: promise,
+//   file: (req, file) => {
+//     return new Promise((resolve, reject) => {
+//       crypto.randomBytes(16, (err, buf) => {
+//         if (err) {
+//           return reject(err);
+//         }
+//         const filename = buf.toString('hex') + path.extname(file.originalname);
+//         const fileInfo = {
+//           filename: filename,
+//           bucketName: 'uploads'
+//         };
+//         resolve(fileInfo);
+//       });
+//     });
+//   }
+// });
 const storage = new GridFsStorage({
   db: promise,
   file: (req, file) => {
     return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const filename = file.originalname;
         const fileInfo = {
           filename: filename,
           bucketName: 'uploads'
         };
         resolve(fileInfo);
-      });
     });
   }
 });
