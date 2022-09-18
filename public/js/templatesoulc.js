9ffab706c60b179a268671c958904a041f3352d1
koyamarten <martinmdmb9@gmail.com>
2022-09-19 01:41:51 +0800

    Initial view full size implementation, fixed modals

--- public/js/templatesoulc.js
+++ public/js/templatesoulc.js
@@ -15,15 +15,25 @@ $(document).ready(() => {
                 console.log(res.pdfFileName);
 
                 const templateView = $('#templateView');
+                const templateViewFullSize = $('#templateViewFullSize');
 
                 templateView.empty();
+                templateViewFullSize.empty();
 
                 const embedPDFView = document.createElement('embed');
                 embedPDFView.setAttribute('src', `/staff/template/${res.pdfFileName}`);
                 embedPDFView.setAttribute('width', '100%');
                 embedPDFView.setAttribute('height', '600px');
 
+                const embedPDFViewFullSize = document.createElement('embed');
+                embedPDFViewFullSize.setAttribute('src', `/staff/template/${res.pdfFileName}`);
+                embedPDFViewFullSize.setAttribute('width', '100%');
+                embedPDFViewFullSize.setAttribute('height', '800px');
+
                 templateView.append(embedPDFView);
+                templateViewFullSize.append(embedPDFViewFullSize)
+
+                $(".full-size-btn").css("display", "block");
 
                 console.log('SUCCESS');
             },
