7d78ea6eb526c187ef3825acb7e13b8da376e2c3
brosoto-cmd <mig_franz_brosoto@dlsu.edu.ph>
2022-09-21 19:30:13 +0800

    Initial search repository and model update (ContractType)
    - Added "code" for contract type
    - Search table columns (repository)

--- models/ContractType.js
+++ models/ContractType.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const ContractTypeSchema = mongoose.Schema({
     name: {
         type: String,
+        code: String,
         required: true
     },
     
--- public/js/oulctablefilter.js
+++ public/js/oulctablefilter.js
@@ -276,8 +276,9 @@ function searchTemplateTable() {
           }
         }
       }
-} 
+}
 
+//Cell color
 $(document).ready(function() {
     var table = document.getElementById("myRequestTable");
     var rows = table.getElementsByTagName("tr");
@@ -296,3 +297,27 @@ $(document).ready(function() {
     viewPending();
 
 });
+
+/* REPOSITORY TABLE */
+function searchRepositoryTable() {
+    var input, filter, table, tr, td, td2, i;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("myRepositoryTable");
+      tr = table.getElementsByTagName("tr");
+      
+      for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0]; // for column one
+        td1 = tr[i].getElementsByTagName("td")[1]; // for column two
+        td2 = tr[i].getElementsByTagName("td")[2];
+    /* ADD columns here that you want you to filter to be used on */
+        if (td) {
+          if ( (td.innerHTML.toUpperCase().indexOf(filter) > -1) || (td1.innerHTML.toUpperCase().indexOf(filter) > -1) ||
+          (td2.innerHTML.toUpperCase().indexOf(filter) > -1) )  {            
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+} 
