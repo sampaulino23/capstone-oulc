b52d3216aab2ebedf22dbc2ed8d13d39a3760315
koyamarten <martinmdmb9@gmail.com>
2022-09-22 23:34:24 +0800

    Contract Repository Design

--- public/js/specificrequest.js
+++ public/js/specificrequest.js
@@ -15,7 +15,7 @@ $(document).ready(() => {
         embedPDFViewFull.setAttribute('height', '800px');
         
         fileView.append(embedPDFView);
-        fileViewFull.append(embedPDFView);
+        fileViewFull.append(embedPDFViewFull);
 
         $.ajax({
             url: "/getcontractversions",
