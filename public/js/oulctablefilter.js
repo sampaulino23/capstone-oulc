83087e22efdbc2a90fdda5dada88c9dccc2104be
koyamarten <martinmdmb9@gmail.com>
2022-10-10 16:25:09 +0800

    implement basic pagination

--- public/js/oulctablefilter.js
+++ public/js/oulctablefilter.js
@@ -355,6 +355,39 @@ $(document).ready(function() {
     var table = document.getElementById("myRequestTable");
     var rows = table.getElementsByTagName("tr");
 
+    var totalRows = $('#table').find('tbody tr:has(td)').length;
+    var recordPerPage = 5;
+    var totalPages = Math.ceil(totalRows / recordPerPage);
+    var $pages = $('<div id="pages"></div>');
+    for (i = 0; i < totalPages; i++) {
+        $('<span class="pageNumber">&nbsp;' + (i + 1) + '</span>').appendTo($pages);
+    }
+    $pages.appendTo('#user-table');
+
+    $('.pageNumber').hover(
+        function() {
+            $(this).addClass('focus');
+        },
+        function() {
+            $(this).removeClass('focus');
+        }
+    );
+
+    $('table').find('tbody tr:has(td)').hide();
+    var tr = $('table tbody tr:has(td)');
+    for (var i = 0; i <= recordPerPage - 1; i++) {
+        $(tr[i]).show();
+    }
+
+    $('span').click(function(event) {
+        $('#table').find('tbody tr:has(td)').hide();
+        var nBegin = ($(this).text() - 1) * recordPerPage;
+        var nEnd = $(this).text() * recordPerPage - 1;
+        for (var i = nBegin; i <= nEnd; i++) {
+            $(tr[i]).show();
+        }
+    });
+
     for (let row of rows) { // `for...of` loops through the NodeList
         cells = row.getElementsByTagName("td");
         var daysGap = cells[1] || null; // gets the 2nd `td` or nothing
@@ -373,7 +406,6 @@ $(document).ready(function() {
     } else if (currentRole == 'Attorney') {
         viewForLegalReview();
     }
-
 });
 
 /* REPOSITORY TABLE */
