42a7af27311c4238f7c7a91cc466fe824634ce65
Ze Long Zhuang <74848471+ZeLongZhuang@users.noreply.github.com>
2022-10-18 22:07:11 +0800

    Auto-update duration

---
+++ public/js/createrequest.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+    // a and b are javascript Date objects
+    function dateDiffInDays(a, b) {
+        const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+        // Discard the time and time-zone information.
+        const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+        const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+      
+        return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+    }
+
+    $('#starteffectivity, #endeffectivity').change(() => {
+
+        const start = $('#starteffectivity').val();
+        const end = $('#endeffectivity').val();
+
+        if (start != '' && end != '') {
+
+            const startDate = new Date(start);
+            const endDate = new Date(end);
+
+            const duration = dateDiffInDays(startDate, endDate);
+            $('#duration').val(duration);
+        }
+
+    });
+});
