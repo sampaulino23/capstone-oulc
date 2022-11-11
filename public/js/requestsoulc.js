0e4c5abf379447da6a67b05a02e4cb3c3d012dcd
koyamarten <martinmdmb9@gmail.com>
2022-11-11 14:00:16 +0800

    Update requestsoulc.js

--- public/js/requestsoulc.js
+++ public/js/requestsoulc.js
@@ -1,13 +1,3 @@
 $(document).ready(() => {
-
-    var role = document.getElementById("currentRole").value;
-    var id = document.getElementById("currentId").value;
     
-    if(role == "Attorney" && id != "6318a6b4c0119ed0b4b6bb82"){
-        document.getElementById("rerouted-tab").style.display = "none";
-    }
-    else {
-        document.getElementById("rerouted-tab").style.display = "";
-    }
-
 });
